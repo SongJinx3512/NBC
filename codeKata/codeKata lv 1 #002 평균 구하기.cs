4eadd168dedf7b6a682a0e8cf1ece6837a3d5645
SongJinx3512 <subsub3512@gmail.com>
2023-09-07 11:10:55 +0900

    repository merge
    repository merge

---
+++ codeKata/codeKata lv 1 #002 평균 구하기.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i=0;i<arr.Length;i++)
+        {
+            answer += arr[i];
+        }
+        answer /= arr.Length;
+        return answer;
+    }
+}
---
+++ codeKata/codeKata lv 1 #008 문자열 정수로 바꾸기
@@ -0,0 +1,9 @@
+public class Solution 
+{
+    public int solution(string s) 
+    {
+        int answer = 0;
+        answer = int.Parse(s);
+        return answer;
+    }
+}
